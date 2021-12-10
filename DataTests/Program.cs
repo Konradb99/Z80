3939f27c9ebf82fe5861183b7e9498d1f44ed375
Krzysztof Gerc <krzysztof.gerc@euvic.pl>
2021-12-10 17:51:57 +0100

    Added base class for bitwise operations

---
+++ DataTests/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using z80.Data.BitManipulationExtensions;
+
+namespace DataTests
+{
+    class Program
+    {
+        public static void Print(BitArray wrapper)
+        {
+            Print(wrapper.Sub(0));
+        }
+
+        public static void Print(BitArraySegment wrapper)
+        {
+            foreach (var val in wrapper)
+            {
+                Console.Write(val ? 1 : 0);
+            }
+            Console.WriteLine();
+        }
+
+        public static void Main(string[] args)
+        {
+            var data = new byte[10];
+            var baw = new BitArray(data);
+            baw.Sub(0).And(baw.Sub(1));
+            Print(baw);
+        }
+    }
+}
---
+++ z80/Data/BitManipulationExtensions/BitArraySegment.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace z80.Data.BitManipulationExtensions
+{
+    public class BitArraySegment : IEnumerable<bool>
+    {
+        public BitArray Array { get; private set; }
+        public int Offset { get; private set; }
+        public int Count { get; private set; }
+
+        public BitArraySegment(BitArray bit)
+            : this(bit, 0)
+        {
+        }
+
+        public BitArraySegment(BitArray bit, int offset)
+            : this(bit, offset, bit.Length - offset)
+        {
+        }
+
+        public BitArraySegment(BitArray bit, int offset, int count)
+        {
+            Array = bit;
+            Offset = offset;
+            Count = count;
+        }
+
+        public int Length => Count;
+
+        public bool Get(int index)
+        {
+            return Array[Offset + index];
+        }
+
+        public void Set(int index, bool value)
+        {
+            Array[Offset + index] = value;
+        }
+
+        public bool this[int index]
+        {
+            get => Get(index);
+            set => Set(index, value);
+        }
+
+        public void And(BitArraySegment bas)
+        {
+            int l = Math.Min(Count, bas.Count);
+            for (int i = 0; i < l; i++)
+            {
+                this[i] &= bas[i];
+            }
+        }
+
+        public void And(BitArray bas)
+        {
+            And(new BitArraySegment(bas));
+        }
+
+        public BitArraySegment Sub(int offset)
+        {
+            return new BitArraySegment(Array, Offset + offset);
+        }
+
+        public BitArraySegment Sub(int offset, int count)
+        {
+            return new BitArraySegment(Array, Offset + offset, count);
+        }
+
+        public BitArraySegment this[int offset, int count] => Sub(offset, count);
+
+        public IEnumerator<bool> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return this[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
