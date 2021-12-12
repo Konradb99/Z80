2141fc5402fdd4a53e70b809da77b99071603be7
Krzysztof Gerc <krzysztof.gerc@euvic.pl>
2021-12-12 19:05:15 +0100

    removed unused code in Program.cs in data tests project

--- DataTests/Program.cs
+++ DataTests/Program.cs
@@ -5,26 +5,9 @@ namespace DataTests
 {
     class Program
     {
-        public static void Print(BitArray wrapper)
-        {
-            Print(wrapper.Sub(0));
-        }
-
-        public static void Print(BitArraySegment array)
-        {
-            foreach (var val in array)
-            {
-                Console.Write(val ? 1 : 0);
-            }
-            Console.WriteLine();
-        }
 
         public static void Main(string[] args)
         {
-            var data = new byte[10];
-            var ba = new BitArray(data);
-            ba.Sub(0).And(ba.Sub(1));
-            Print(ba);
         }
     }
 }
